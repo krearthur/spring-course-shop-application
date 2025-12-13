8a14804c1a1abdb777ae02455523fcffdf25707b
krearthur <artgrohe@gmail.com>
2025-12-13 17:05:43 +0100

    55. Defining Endpoints for Getting, Deleting and Searching Products

---
+++ src/main/java/de/krearthur/shop/service/ProductService.java
@@ -0,0 +1,80 @@
+package de.krearthur.shop.service;
+
+import de.krearthur.shop.dto.ProductRequest;
+import de.krearthur.shop.dto.ProductResponse;
+import de.krearthur.shop.model.Product;
+import de.krearthur.shop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = new Product();
+        updateProductFromRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        ProductResponse response = new ProductResponse();
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setActive(product.getActive());
+        response.setCategory(product.getCategory());
+        response.setDescription(product.getDescription());
+        response.setPrice(product.getPrice());
+        response.setImageUrl(product.getImageUrl());
+        response.setStockQuantity(product.getStockQuantity());
+
+        return response;
+    }
+
+    private void updateProductFromRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setCategory(productRequest.getCategory());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+    }
+
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, productRequest);
+                    Product savedProduct = productRepository.save(existingProduct);
+                    return mapToProductResponse(savedProduct);
+                });
+    }
+
+    public List<ProductResponse> getActiveProducts() {
+        return productRepository.findByActiveTrue().stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    public boolean deleteProduct(Long id) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setActive(false);
+                    productRepository.save(product);
+                    return true;
+                }).orElse(false);
+    }
+
+    public List<ProductResponse> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword)
+                .stream().map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+}
