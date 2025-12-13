2dc9ac1fffb8291edc65d00b14905a43fc6577f5
krearthur <artgrohe@gmail.com>
2025-12-13 17:17:05 +0100

    56. Setting up User Cart

---
+++ src/main/java/de/krearthur/shop/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package de.krearthur.shop.repository;
+
+import de.krearthur.shop.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
